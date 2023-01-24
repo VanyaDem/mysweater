558d1a418143f7a3f587907956d152f799812d3d
Vanya Demydenko <educationvanyadem@gmail.com>
2023-01-25 01:02:32 +0200

    Added new dependency Mustache. She is change thymeleaf.

--- src/main/java/com/example/mysweater/GreetingController.java
+++ src/main/java/com/example/mysweater/GreetingController.java
@@ -1,16 +1,23 @@
 package com.example.mysweater;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class GreetingController {
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
+                           Map<String , Object> model) {
+        model.put("name", name);
         return "greeting";
     }
 
+    @GetMapping("/")
+    public String main (Map<String ,Object> model){
+        model.put("some","developer");
+        return"main";
+    }
 }

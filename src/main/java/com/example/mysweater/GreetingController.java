dad9eff9989ff317a43ae888218e0989d96a88ee
Vanya Demydenko <educationvanyadem@gmail.com>
2023-01-25 02:32:06 +0200

    Change main controller and main template

--- src/main/java/com/example/mysweater/GreetingController.java
+++ src/main/java/com/example/mysweater/GreetingController.java
@@ -1,5 +1,8 @@
 package com.example.mysweater;
 
+import com.example.mysweater.domain.Message;
+import com.example.mysweater.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +11,9 @@ import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepo  messageRepo;
+
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
                            Map<String , Object> model) {
@@ -17,7 +23,9 @@ public class GreetingController {
 
     @GetMapping("/")
     public String main (Map<String ,Object> model){
-        model.put("some","developer");
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages",messages);
         return"main";
     }
 }

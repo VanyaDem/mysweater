446b4956416b6196d54b23f984767464977ab8e0
Vanya Demydenko <educationvanyadem@gmail.com>
2023-01-25 20:44:12 +0200

    Added the ability to register

--- src/main/java/com/example/mysweater/config/WebSecurityConfig.java
+++ src/main/java/com/example/mysweater/config/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
    protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                        .antMatchers("/").permitAll()
+                        .antMatchers("/","/registration").permitAll()
                         .anyRequest().authenticated()
                 .and()
                         .formLogin()

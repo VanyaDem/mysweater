0e820e7b55e771ef304a5496c8a198a3abfb4979
Vanya Demydenko <educationvanyadem@gmail.com>
2023-01-25 03:36:58 +0200

    Added the ability to filter messages

--- src/main/java/com/example/mysweater/GreetingController.java
+++ src/main/java/com/example/mysweater/GreetingController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -44,5 +45,18 @@ public class GreetingController {
 
         model.put("messages",messages);
         return"main";
+    }
+
+    @PostMapping("/filter")
+    public String filter(@RequestParam String filter,
+                         Map<String ,Object> model){
+        Iterable<Message> messages;
+        if(filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        }else{
+            messages = messageRepo.findAll();
+        }
+        model.put("messages" , messages);
+        return "main";
     }
 }
--- src/main/java/com/example/mysweater/repos/MessageRepo.java
+++ src/main/java/com/example/mysweater/repos/MessageRepo.java
@@ -3,6 +3,9 @@ package com.example.mysweater.repos;
 import com.example.mysweater.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Long> {
 
+       List<Message> findByTag(String tag);
 }
